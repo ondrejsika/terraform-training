2623b5ef051994eca2a2960de24ae11f983dd2f5
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-18 17:28:05 +0200

    examples/first-step - add first step example (webserver)

---
+++ examples/first-step/terraform.tf
@@ -0,0 +1,20 @@
+provider "digitalocean" {}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "default"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "digitalocean_droplet" "example" {
+  image    = "debian-10-x64"
+  name     = "example"
+  region   = "fra1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [
+    digitalocean_ssh_key.default.fingerprint
+  ]
+}
+
+output "ip_addr" {
+  value = digitalocean_droplet.example.ipv4_address
+}
