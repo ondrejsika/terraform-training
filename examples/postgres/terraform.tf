51ce4dbf5ec91041f9493382a2903b436d0ecb6d
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-26 09:24:54 +0200

    examples/postgres - add example with managed postgres on Digital Ocean

---
+++ examples/postgres/terraform.tf
@@ -0,0 +1,34 @@
+variable "base_domain" {}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "digitalocean" {}
+
+data "digitalocean_ssh_key" "default" {
+  name = "default"
+}
+
+data "digitalocean_domain" "default" {
+  name = var.base_domain
+}
+
+resource "digitalocean_database_cluster" "postgres" {
+  name       = "postgres"
+  engine     = "pg"
+  version    = "11"
+  size       = "db-s-1vcpu-1gb"
+  region     = "fra1"
+  node_count = 1
+}
+
+output "uri" {
+  value = digitalocean_database_cluster.postgres.uri
+  sensitive = true
+}
