6634baa34d2002a74c451078c55b8bcfea8a5e8e
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-15 07:48:37 +0200

    feat(examples/for_each): Add for_each example

---
+++ examples/for_each/terraform.tf
@@ -0,0 +1,61 @@
+variable "base_domain" {}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "digitalocean" {}
+
+data "digitalocean_ssh_key" "default" {
+  name = "default"
+}
+
+data "digitalocean_domain" "default" {
+  name = var.base_domain
+}
+
+locals {
+  vms = {
+    foo = {
+      image = "debian-10-x64"
+      size  = "s-1vcpu-1gb"
+    }
+    bar = {
+      image = "debian-10-x64"
+      size  = "s-1vcpu-1gb"
+    }
+  }
+}
+
+resource "digitalocean_droplet" "example" {
+  for_each = locals.vms
+
+  image  = each.value.image
+  name   = "example-${each.key}"
+  region = "fra1"
+  size   = each.value.size
+  ssh_keys = [
+    data.digitalocean_ssh_key.default.fingerprint
+  ]
+}
+
+resource "digitalocean_record" "example" {
+  for_each = locals.vms
+
+
+  domain = data.digitalocean_domain.default.name
+  type   = "A"
+  name   = digitalocean_droplet.example[each.key].name
+  value  = digitalocean_droplet.example[each.key].ipv4_address
+}
+
+output "domains" {
+  value = [
+    for instance in digitalocean_record.example : instance.fqdn
+  ]
+}
