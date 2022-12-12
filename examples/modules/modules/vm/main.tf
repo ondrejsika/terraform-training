016d6c6b4e0abef9fb493d2010fb0d4ce18ceeee
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-12 18:32:47 +0100

    feat(examples/modules): Use "this" as name of object in module

--- examples/modules/modules/vm/main.tf
+++ examples/modules/modules/vm/main.tf
@@ -17,7 +17,7 @@ terraform {
   required_version = ">= 0.13"
 }
 
-resource "digitalocean_droplet" "default" {
+resource "digitalocean_droplet" "this" {
   image  = var.image
   name   = var.name
   region = "fra1"
@@ -27,17 +27,17 @@ resource "digitalocean_droplet" "default" {
   ]
 }
 
-resource "digitalocean_record" "default" {
+resource "digitalocean_record" "this" {
   domain = var.tf_domain.name
   type   = "A"
-  name   = digitalocean_droplet.default.name
-  value  = digitalocean_droplet.default.ipv4_address
+  name   = digitalocean_droplet.this.name
+  value  = digitalocean_droplet.this.ipv4_address
 }
 
 output "ipv4_address" {
-  value = digitalocean_droplet.default.ipv4_address
+  value = digitalocean_droplet.this.ipv4_address
 }
 
 output "fqdn" {
-  value = digitalocean_record.default.fqdn
+  value = digitalocean_record.this.fqdn
 }
