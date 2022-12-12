4549fd8aff53974646e27306a4e74a41660f0495
Ondrej Sika <ondrej@ondrejsika.com>
2022-12-12 18:37:27 +0100

    feat(examples/gitlab_state_backend): Create example og Gitlab backend for Terraform state

---
+++ examples/gitlab_state_backend/terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "http" {}
+
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+variable "base_domain" {}
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_domain" "default" {
+  name = var.base_domain
+}
+
+resource "digitalocean_record" "foo" {
+  domain = data.digitalocean_domain.default.name
+  type   = "CNAME"
+  name   = "gitlab-state-backend-foo"
+  value  = "example.com."
+}
+
+resource "digitalocean_record" "bar" {
+  domain = data.digitalocean_domain.default.name
+  type   = "CNAME"
+  name   = "gitlab-state-backend-foo"
+  value  = "example.com."
+}
