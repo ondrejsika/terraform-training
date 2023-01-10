ca980521e697df1b2420b662c1e0ca3e86739553
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-10 16:50:28 +0100

    feat(examples/condition): Add example of if condition (implemented using count)

---
+++ examples/condition/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/digitalocean/digitalocean" {
+  version = "2.25.2"
+  hashes = [
+    "h1:z33HsVmHl5PH8NI1kY07+1Gc8+gLxGDa6xsKpwdUbJc=",
+  ]
+}
---
+++ examples/condition/terraform.tf
@@ -0,0 +1,33 @@
+variable "base_domain" {}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "digitalocean" {}
+
+data "digitalocean_ssh_key" "default" {
+  name = "default"
+}
+
+data "digitalocean_domain" "default" {
+  name = var.base_domain
+}
+
+locals {
+  enable = false
+}
+
+resource "digitalocean_record" "example" {
+  count = local.enable ? 1 : 0
+
+  domain = data.digitalocean_domain.default.name
+  type   = "A"
+  name   = "enable"
+  value  = "1.1.1.1"
+}
