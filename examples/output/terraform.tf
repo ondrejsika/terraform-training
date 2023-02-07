7630e8fdbbe7ae1427c2047a1818af0c14ac1f28
Ondrej Sika <ondrej@ondrejsika.com>
2023-02-07 10:13:29 +0100

    feat(examples/output): Add list & map outputs

--- examples/output/terraform.tf
+++ examples/output/terraform.tf
@@ -6,3 +6,17 @@ output "secret" {
   value     = "Sensitive Output"
   sensitive = true
 }
+
+output "list" {
+  value = [
+    "foo",
+    "bar",
+  ]
+}
+
+output "map" {
+  value = {
+    hello = "ahoj"
+    world = "svete"
+  }
+}
